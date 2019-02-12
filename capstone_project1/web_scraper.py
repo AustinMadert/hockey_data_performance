d2178020f652ebea25dc1cc26f53004c5abb4a6a
Austin Madert <austinmadert@Austins-MacBook-Pro.local>
2019-02-12 15:28:45 -0600

    Wire framed scraper

--- capstone_project1/web_scraper.py
+++ capstone_project1/web_scraper.py
@@ -1,12 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
+from pymongo import MongoClient
+import random, time
 
+def scrape(urls, stall):
+    for ind, lst in enumerate(urls):
+        try:
+            website = requests.get(str(ind))
+        except:
+            print("Requests error with url #" + str(ind))
+        try:
+            data = parser(website)
+        except:
+            print("Parse error with url " + str(ind))
+        try:
+            store(data)
+        except:
+            print("Store error with url " + str(ind))
+        print("URL data successfully stored in database.")
+        time.sleep(stall)
+    pass
 
-class Scrape(object):
-    
-    def __init__(self, sites=[], mongo_db=None, stall=15):
-        self._sites = sites
-        self._mongo_db = mongo_db
-        self._stall = stall
+def parser(website):
+    soup = BeautifulSoup(website.text, 'html.parser')
 
-    
+    return data
+
+def store():
+
+    #mongo_connect.insert_one()
+    pass
